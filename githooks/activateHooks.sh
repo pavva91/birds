6337f249dc03f2b35e787920a84d436b9e8814dc
pavva91 <valerio.mattioli@proton.me>
2024-05-08 11:54:17 +0200

    try push

--- githooks/activateHooks.sh
+++ githooks/activateHooks.sh
@@ -4,4 +4,5 @@
 # substitute /absolute/path/to/repo/ with your own absolute path
 # e.g. /home/youruser/github/birds/
 ln -s /absolute/path/to/repo/githooks/pre-commit.sh /absolute/path/to/repo/.git/hooks/pre-commit
+ln -s /absolute/path/to/repo/githooks/pre-push.sh /absolute/path/to/repo/.git/hooks/pre-push
 
