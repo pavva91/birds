d40bb90d22fa518ed5663ae6dd7c2035005222b2
pavva91 <valerio.mattioli@proton.me>
2024-05-08 11:52:33 +0200

    add pre-push hook

--- githooks/pre-commit.sh
+++ githooks/pre-commit.sh
@@ -6,24 +6,21 @@ echo "Pre-commit Git Hook: Format (prettier) staged ts files"
 STAGED_TS_FILES=$(git diff --cached --name-only -- '*.ts')
 
 if $STAGED_TS_FILES '==' ""; then
-    echo "No TS Files to Update"
+	echo "No TS Files to Update"
 else
-    echo "Update TS Files:"
-    for ts_file in $STAGED_TS_FILES;
-    do
-        npx prettier "$ts_file" --write
-        git add "$ts_file"
-        echo "formatted: $ts_file"
-    done
+	echo "Update TS Files:"
+	for ts_file in $STAGED_TS_FILES; do
+		npx prettier "$ts_file" --write
+		git add "$ts_file"
+		echo "formatted: $ts_file"
+	done
 
-echo "Run linter (eslint)"
-    for ts_file in $STAGED_TS_FILES;
-    do
-        npx eslint "$ts_file"
-        echo "$ts_file"
-    done
+	echo "Run linter (eslint)"
+	for ts_file in $STAGED_TS_FILES; do
+		npx eslint "$ts_file"
+		echo "$ts_file"
+	done
 fi
 
 # TODO: go
 # TODO: java
-
---
+++ githooks/pre-push.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# ts
+echo "Pre-push Git Hook: Run Test Suite"
+npm run test
+
+# TODO: go
+# TODO: java
