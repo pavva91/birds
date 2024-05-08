6c62adc016fe2294d54c2a278abd49f6b271ccb4
pavva91 <valerio.mattioli@proton.me>
2024-05-08 11:44:05 +0200

    add githook to format and run linter at pre-commit

---
+++ githooks/activateHooks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# NOTE: Use absolute path
+# substitute /absolute/path/to/repo/ with your own absolute path
+# e.g. /home/youruser/github/birds/
+ln -s /absolute/path/to/repo/githooks/pre-commit.sh /absolute/path/to/repo/.git/hooks/pre-commit
+
