4b2ca60e97d09d227af4015dc0131941845fda54
pavva91 <valerio.mattioli@proton.me>
2024-05-08 12:53:00 +0200

    fix bash

--- githooks/pre-commit.sh
+++ githooks/pre-commit.sh
@@ -5,20 +5,20 @@ echo "Pre-commit Git Hook: Format (prettier) staged ts files"
 
 STAGED_TS_FILES=$(git diff --cached --name-only -- '*.ts')
 
-if $STAGED_TS_FILES '==' ""; then
+if [ "$STAGED_TS_FILES" '==' "" ]; then
 	echo "No TS Files to Update"
 else
-	echo "Update TS Files:"
+	echo "Run formatter (prettier)"
 	for ts_file in $STAGED_TS_FILES; do
 		npx prettier "$ts_file" --write
 		git add "$ts_file"
-		echo "formatted: $ts_file"
 	done
 
 	echo "Run linter (eslint)"
 	for ts_file in $STAGED_TS_FILES; do
-		npx eslint "$ts_file"
+		npx eslint "$ts_file" --fix
 		echo "$ts_file"
+		git add "$ts_file"
 	done
 fi
 
